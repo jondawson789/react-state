7537202743fe20fef89035094ed6ec22e50d8701
jdawson <jonathan.dawson789@mgmail.com>
2022-05-11 08:21:35 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EightBall from './Eightball'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall/>
+    </div>
+  );
+}
+
+export default App;
